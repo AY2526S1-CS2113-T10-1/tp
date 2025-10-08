6094875352107a39d3ca68976d62462eaf09136f
Emannuel Tan Jing Yue <emannueltanjingyue@gmail.com>
2025-10-08 20:28:04 +0800

    Add AddLoanCommandWrongFormatException.java

---
+++ src/main/java/exceptions/AddLoanCommandWrongFormatException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+/**
+ * Exception thrown if add loan command has empty fields or missing sub commands or sub commands in wrong order
+ */
+public class AddLoanCommandWrongFormatException extends Exception {
+    @Override
+    public String getMessage() {
+        return "Add Loan Command is in the wrong format!!! Please try again with the format:\n" +
+                "add loan d/<DESCRIPTION> a/<AMOUNT_LOANED> r/<LOAN_RETURN_DATE>\n" +
+                "where <LOAN_RETURN_DATE> is of format 'DD-mm-YYYY HH:mm";
+    }
+}
