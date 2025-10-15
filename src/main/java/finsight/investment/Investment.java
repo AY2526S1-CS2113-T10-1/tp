26baff2266b9f8df893054eb222998c64cbaf9a0
Rezelix <lkeshnair346@gmail.com>
2025-10-15 13:59:52 +0800

    Add toString method and recurring deposit member for monthly investment deposits

--- src/main/java/finsight/investment/Investment.java
+++ src/main/java/finsight/investment/Investment.java
@@ -2,18 +2,28 @@ package finsight.investment;
 
 import finsight.investment.exceptions.AddInvestmentWrongNumberFormatException;
 
+
 public class Investment {
     protected String description;
     protected Double investmentAmount;
     protected String investmentType;
-    protected boolean investmentRate;
+    protected int investmentDateOfMonth;
 
-    public Investment(String description, String investmentAmount) throws AddInvestmentWrongNumberFormatException {
+    public Investment(String description, String investmentAmount, String investmentDateOfMonth) throws AddInvestmentWrongNumberFormatException {
         this.description = description;
         try {
             this.investmentAmount = Double.parseDouble(investmentAmount);
+            this.investmentDateOfMonth = Integer.parseInt(investmentDateOfMonth);
         } catch (NumberFormatException e) {
             throw new AddInvestmentWrongNumberFormatException();
         }
+    }
+
+    @Override
+    public String toString() {
+        String output = "Investment Description: " + description;
+        output += "\nAmount: " + investmentAmount;
+        output += "\nRecurring Deposit Date of Month: " + investmentDateOfMonth;
+        return output;
     }
 }
