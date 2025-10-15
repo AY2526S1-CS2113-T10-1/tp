831841b5aed2ab743b986b394b5f397cee1a19de
Rezelix <lkeshnair346@gmail.com>
2025-10-15 11:38:21 +0800

    Add methods to Investment List class to list, add and delete from List

--- src/main/java/finsight/investment/investmentlist/InvestmentList.java
+++ src/main/java/finsight/investment/investmentlist/InvestmentList.java
@@ -1,4 +1,40 @@
 package finsight.investment.investmentlist;
 
+import finsight.investment.Investment;
+import finsight.ui.Ui;
+
+import java.util.ArrayList;
+
 public class InvestmentList {
+    protected ArrayList<Investment> investmentList;
+    protected Ui ui;
+
+    public InvestmentList(ArrayList<Investment> investmentList, Ui ui) {
+        this.investmentList = investmentList;
+        this.ui = ui;
+    }
+
+    public InvestmentList(Ui ui) {
+        this.ui = ui;
+    }
+
+    public void listAllInvestments() {
+        ui.printAllInvestments();
+    }
+
+    public void addInvestment(Investment investment) {
+        investmentList.add(investment);
+        ui.printAddInvestmentOutput();
+    }
+
+    public void deleteInvestment(Investment indexToDelete) {
+        investmentList.remove(indexToDelete);
+        ui.printDeleteInvestmentOutput();
+    }
+
+    public int getSize() {
+        return investmentList.size();
+    }
+
+
 }
