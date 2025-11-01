a57f4f2166cc3249662abe74deee8c77c6b16310
Emannuel Tan Jing Yue <emannueltanjingyue@gmail.com>
2025-11-01 19:47:36 +0800

    Update Error Message

--- src/main/java/finsight/loan/exceptions/EditLoanCommandPastDateUsedException.java
+++ src/main/java/finsight/loan/exceptions/EditLoanCommandPastDateUsedException.java
@@ -9,6 +9,6 @@ public class EditLoanCommandPastDateUsedException extends Exception {
         return "Loan Return Date is in the past. Please try again with the format:\n" +
                 "edit loan <INDEX> d/<DESCRIPTION> a/<AMOUNT_LOANED> r/<LOAN_RETURN_DATE_AND_TIME>\n" +
                 "where <LOAN_RETURN_DATE_AND_TIME> is of format 'dd-MM-yyyy HH:mm' and\n" +
-                "<AMOUNT_LOANED> is only positive integer or float";
+                "<AMOUNT_LOANED> is only positive integer or float of at least 1 cent";
     }
 }
